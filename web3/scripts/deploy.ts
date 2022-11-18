5ba00743158ca142bbef30ff17654b80c76c2f3b
Isaac Tsibu-Darko <isaactsibudarko@gmail.com>
2022-11-18 09:57:57 +0000

    web3 contract set up and deployed and frontend set up using vite

---
+++ web3/scripts/deploy.ts
@@ -0,0 +1,27 @@
+import { ethers } from 'hardhat';
+import console from 'console';
+
+const _metadataUri = 'https://gateway.pinata.cloud/ipfs/https://gateway.pinata.cloud/ipfs/QmX2ubhtBPtYw75Wrpv6HLb1fhbJqxrnbhDo1RViW3oVoi';
+
+async function deploy(name: string, ...params: [string]) {
+  const contractFactory = await ethers.getContractFactory(name);
+
+  return await contractFactory.deploy(...params).then((f) => f.deployed());
+}
+
+async function main() {
+  const [admin] = await ethers.getSigners();
+  
+  console.log(`Deploying a smart contract...`);
+
+  const AVAXLegends = (await deploy('AVAXLegends', _metadataUri)).connect(admin);
+
+  console.log({ AVAXLegends: AVAXLegends.address });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  });
